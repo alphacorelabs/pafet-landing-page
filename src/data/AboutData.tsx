9cf1ba85f226f6a726e30e21c78e74b45a801292
maxwell <techgbenga@gmail.com>
2025-05-18 22:16:51 +0100

    updated phone and email

--- src/data/AboutData.tsx
+++ src/data/AboutData.tsx
@@ -1,50 +1,48 @@
-import { FaRegEnvelope } from "react-icons/fa6"
-import { MdOutlinePhone } from "react-icons/md"
-
+import { FaRegEnvelope } from "react-icons/fa6";
+import { MdOutlinePhone } from "react-icons/md";
 
 export const valuesData = [
-    {
-        title: "Transparency",
-        content: "We believe in transparency and empowerment. We promise no hidden fees, ever.",
-        icon: "https://res.cloudinary.com/doqvfemo3/image/upload/f_auto,q_auto/v1/Pafet/okwdxqwgtmswqfzs0t9s",
-    },
+  {
+    title: "Transparency",
+    content: "We believe in transparency and empowerment. We promise no hidden fees, ever.",
+    icon: "https://res.cloudinary.com/doqvfemo3/image/upload/f_auto,q_auto/v1/Pafet/okwdxqwgtmswqfzs0t9s",
+  },
 
-    {
-        title: "Security",
-        content: "Your money’s safe. We work only with fully licensed, regulated financial partners.",
-        icon: "https://res.cloudinary.com/doqvfemo3/image/upload/f_auto,q_auto/v1/Pafet/d16ljx3wsk0eyt4k4mys",
-    },
+  {
+    title: "Security",
+    content: "Your money’s safe. We work only with fully licensed, regulated financial partners.",
+    icon: "https://res.cloudinary.com/doqvfemo3/image/upload/f_auto,q_auto/v1/Pafet/d16ljx3wsk0eyt4k4mys",
+  },
 
-    {
-        title: "Empowerment",
-        content: "We champion your hustle: We make it easier to earn and spend freely no matter where you are.",
-        icon: "https://res.cloudinary.com/doqvfemo3/image/upload/f_auto,q_auto/v1/Pafet/zf3rxh8wl53vwyjfcnnl",
-    },
+  {
+    title: "Empowerment",
+    content: "We champion your hustle: We make it easier to earn and spend freely no matter where you are.",
+    icon: "https://res.cloudinary.com/doqvfemo3/image/upload/f_auto,q_auto/v1/Pafet/zf3rxh8wl53vwyjfcnnl",
+  },
 
-    {
-        title: "Simplicity",
-        content: "We strip out the complications. Everything you need, in one easy flow.",
-        icon: "https://res.cloudinary.com/doqvfemo3/image/upload/f_auto,q_auto/v1/Pafet/n7cfdf754yuymitw0iwv",
-    },
+  {
+    title: "Simplicity",
+    content: "We strip out the complications. Everything you need, in one easy flow.",
+    icon: "https://res.cloudinary.com/doqvfemo3/image/upload/f_auto,q_auto/v1/Pafet/n7cfdf754yuymitw0iwv",
+  },
 
-    {
-        title: "Ownership",
-        content: "You're in charge. We just hand you the tools to move faster, smarter, and without limits.",
-        icon: "https://res.cloudinary.com/doqvfemo3/image/upload/f_auto,q_auto/v1/Pafet/tenco7fe5gjl6wavmgb6",
-    },
-]
-
+  {
+    title: "Ownership",
+    content: "You're in charge. We just hand you the tools to move faster, smarter, and without limits.",
+    icon: "https://res.cloudinary.com/doqvfemo3/image/upload/f_auto,q_auto/v1/Pafet/tenco7fe5gjl6wavmgb6",
+  },
+];
 
 export const contactInfo = [
-    {
-        title: "Email address",
-        content: "example@mail.com",
-        icon: FaRegEnvelope,
-    },
+  {
+    title: "Email address",
+    content: "hello@usePafet.com",
+    icon: FaRegEnvelope,
+  },
 
-    {
-        title: "Call line",
-        content: "+44-01234-561",
-        icon: MdOutlinePhone,
-    },
-]
+  {
+    title: "Call line",
+    content: "+1(302) 564-2246",
+    icon: MdOutlinePhone,
+  },
+];
--- src/features/Support/StandBy.tsx
+++ src/features/Support/StandBy.tsx
@@ -33,230 +33,135 @@ import MainHeading from "../../ui/MainHeading";
 import { FaRegEnvelope } from "react-icons/fa6";
 import { MdOutlinePhone } from "react-icons/md";
 
-
 function StandBy() {
-
   const isDesktop = useBreakpointValue({ base: false, lg: true });
 
   return (
-    <Stack
-      w="full"
-      justify="start"
-      align="start"
-      py={["4rem", "4rem", "6rem"]}
-      bgColor="grey.100"
-    >
-        <ContainerLayout>
-            <VStack
-              w="full"
-              justify="center"
-              align="center"
-              py={["40px", "40px", "100px"]}
-              px={["20px", "20px", "128px"]}
-              gap="24px"
+    <Stack w="full" justify="start" align="start" py={["4rem", "4rem", "6rem"]} bgColor="grey.100">
+      <ContainerLayout>
+        <VStack w="full" justify="center" align="center" py={["40px", "40px", "100px"]} px={["20px", "20px", "128px"]} gap="24px">
+          <VStack w={["full", "full", "60%"]} mx={["initial", "initial", "auto"]} justify="center" align="center" gap="24px">
+            <Heading fontSize={[28, 28, 56]} fontWeight={600} lineHeight="120%" color="grey.800" letterSpacing="-4%" textAlign="center">
+              Still need help? Our team is on standby
+            </Heading>
+          </VStack>
+
+          <Text
+            fontSize={[16, 16, 20]}
+            fontWeight={[400, 400, 500]}
+            lineHeight="160%"
+            color="grey.600"
+            letterSpacing="0%"
+            textAlign="center"
+          >
+            Message us in the app or send a quick email to
+            <Text
+              as="span"
+              fontSize={[16, 16, 20]}
+              fontWeight={[400, 400, 600]}
+              mx="1"
+              lineHeight="160%"
+              color="brand.100"
+              letterSpacing="0%"
+              textAlign="center"
             >
-                <VStack
-                    w={["full", "full", "60%"]}
-                    mx={["initial", "initial", "auto"]}
-                    justify="center"
-                    align="center"
-                    gap="24px"
-                >
-                    <Heading
-                      fontSize={[28, 28, 56]}
-                      fontWeight={600}
-                      lineHeight="120%"
-                      color="grey.800"
-                      letterSpacing="-4%"
-                      textAlign="center"
-                    >
-                        Still need help? Our team is on standby
-                    </Heading>
+              hello@usePafet.com
+            </Text>
+          </Text>
 
-                </VStack>
+          <HStack
+            w={["full", "full", "80%"]}
+            mx={["initial", "initial", "auto"]}
+            justify="space-between"
+            align="center"
+            gap="40px"
+            wrap={["wrap", "wrap", "nowrap"]}
+            mt="40px"
+          >
+            <HStack
+              w="full"
+              justify={["center", "center", "start"]}
+              align="center"
+              gap={["4px", "4px", "10px"]}
+              wrap={["wrap", "wrap", "nowrap"]}
+            >
+              <HStack w="fit-content" justify="start" align="center" gap="10px">
+                <Icon as={FaRegEnvelope} fontSize="24px" color="grey.600" />
 
-                <Text
-                    fontSize={[16, 16, 20]}
-                    fontWeight={[400, 400, 500]}
-                    lineHeight="160%"
-                    color="grey.600"
-                    letterSpacing="0%"
-                    textAlign="center"
-                >
-                    Message us in the app or send a quick email to 
-                    <Text
-                        as="span"
-                        fontSize={[16, 16, 20]}
-                        fontWeight={[400, 400, 600]}
-                        mx="1"
-                        lineHeight="160%"
-                        color="brand.100"
-                        letterSpacing="0%"
-                        textAlign="center"
-                    >
-                        Support@pafet.com
-                    </Text>
+                <Text fontSize={[18, 18, 20]} fontWeight={500} lineHeight="160%" color="grey.600" letterSpacing="0%" textAlign="center">
+                  Email address:
                 </Text>
+              </HStack>
 
-                <HStack
-                  w={["full", "full", "80%"]}
-                  mx={["initial", "initial", "auto"]}
-                  justify="space-between"
-                  align="center"
-                  gap="40px"
-                  wrap={["wrap", "wrap", "nowrap"]}
-                  mt="40px"
-                >
-                    <HStack
-                      w="full"
-                      justify={["center", "center", "start"]}
-                      align="center"
-                      gap={["4px", "4px", "10px"]}
-                      wrap={["wrap", "wrap", "nowrap"]}
-                    >
-                      <HStack
-                          w="fit-content"
-                          justify="start"
-                          align="center"
-                          gap="10px"
-                      >
-                          <Icon
-                            as={FaRegEnvelope}
-                            fontSize="24px"
-                            color="grey.600"
-                          />
+              <Text
+                fontSize={[18, 18, 20]}
+                fontWeight={600}
+                mx="1"
+                lineHeight="160%"
+                color="brand.100"
+                letterSpacing="0%"
+                textAlign="center"
+              >
+                hello@usePafet.com
+              </Text>
+            </HStack>
 
-                          <Text
-                              fontSize={[18, 18, 20]}
-                              fontWeight={500}
-                              lineHeight="160%"
-                              color="grey.600"
-                              letterSpacing="0%"
-                              textAlign="center"
-                          >
-                              Email address:
-                          </Text>
+            {isDesktop ? (
+              <HStack
+                w="full"
+                justify={["center", "center", "end"]}
+                align="center"
+                gap={["4px", "4px", "10px"]}
+                wrap={["wrap", "wrap", "nowrap"]}
+              >
+                <HStack w="fit-content" justify="start" align="center" gap="10px">
+                  <Icon as={MdOutlinePhone} fontSize="24px" color="grey.600" />
 
-                      </HStack>
-
-                      <Text
-                          fontSize={[18, 18, 20]}
-                          fontWeight={600}
-                          mx="1"
-                          lineHeight="160%"
-                          color="brand.100"
-                          letterSpacing="0%"
-                          textAlign="center"
-                      >
-                          example@mail.com
-                      </Text>
-
-                    </HStack>
-
-                    {isDesktop ?
-                        <HStack
-                          w="full"
-                          justify={["center", "center", "end"]}
-                          align="center"
-                          gap={["4px", "4px", "10px"]}
-                          wrap={["wrap", "wrap", "nowrap"]}
-                        >
-                          <HStack
-                              w="fit-content"
-                              justify="start"
-                              align="center"
-                              gap="10px"
-                          >
-                              <Icon
-                                as={MdOutlinePhone}
-                                fontSize="24px"
-                                color="grey.600"
-                              />
-
-                              <Text
-                                  fontSize={[18, 18, 20]}
-                                  fontWeight={500}
-                                  lineHeight="160%"
-                                  color="grey.600"
-                                  letterSpacing="0%"
-                                  textAlign="center"
-                              >
-                                  Call line:
-                              </Text>
-
-                          </HStack>
-
-                          <Text
-                              fontSize={[18, 18, 20]}
-                              fontWeight={600}
-                              mx="1"
-                              lineHeight="160%"
-                              color="brand.100"
-                              letterSpacing="0%"
-                              textAlign="center"
-                          >
-                              +44-01234-561
-                          </Text>
-
-                        </HStack>
-
-                        :
-
-                        <VStack
-                          w="full"
-                          justify="center"
-                          align="center"
-                        >
-                          <HStack
-                              w="fit-content"
-                              justify="start"
-                              align="center"
-                              gap="10px"
-                          >
-                              <Icon
-                                as={MdOutlinePhone}
-                                fontSize="24px"
-                                color="grey.600"
-                              />
-    
-                              <Text
-                                  fontSize={[18, 18, 20]}
-                                  fontWeight={500}
-                                  lineHeight="160%"
-                                  color="grey.600"
-                                  letterSpacing="0%"
-                                  textAlign="center"
-                              >
-                                  Call line:
-                              </Text>
-    
-                          </HStack>
-  
-                        <Text
-                            fontSize={[18, 18, 20]}
-                            fontWeight={600}
-                            mx="1"
-                            lineHeight="160%"
-                            color="brand.100"
-                            letterSpacing="0%"
-                            textAlign="center"
-                        >
-                            +44-01234-561
-                        </Text>
-  
-                      </VStack>
-                    
-                    }
-
-
-
+                  <Text fontSize={[18, 18, 20]} fontWeight={500} lineHeight="160%" color="grey.600" letterSpacing="0%" textAlign="center">
+                    Call line:
+                  </Text>
                 </HStack>
 
-            </VStack>
-        </ContainerLayout>
+                <Text
+                  fontSize={[18, 18, 20]}
+                  fontWeight={600}
+                  mx="1"
+                  lineHeight="160%"
+                  color="brand.100"
+                  letterSpacing="0%"
+                  textAlign="center"
+                >
+                  +1(302) 564-2246
+                </Text>
+              </HStack>
+            ) : (
+              <VStack w="full" justify="center" align="center">
+                <HStack w="fit-content" justify="start" align="center" gap="10px">
+                  <Icon as={MdOutlinePhone} fontSize="24px" color="grey.600" />
 
+                  <Text fontSize={[18, 18, 20]} fontWeight={500} lineHeight="160%" color="grey.600" letterSpacing="0%" textAlign="center">
+                    Call line:
+                  </Text>
+                </HStack>
+
+                <Text
+                  fontSize={[18, 18, 20]}
+                  fontWeight={600}
+                  mx="1"
+                  lineHeight="160%"
+                  color="brand.100"
+                  letterSpacing="0%"
+                  textAlign="center"
+                >
+                  +1(302) 564-2246
+                </Text>
+              </VStack>
+            )}
+          </HStack>
+        </VStack>
+      </ContainerLayout>
     </Stack>
-  )
+  );
 }
 
-export default StandBy
+export default StandBy;
